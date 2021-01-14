87d0ba1c03a6cae96693563f468e1365c3a5c666
Aline Denizot <aline.denizot2@gmail.com>
2021-01-14 14:50:29 +0100

    création d'un form d'inscription

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -12,6 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\RegistrationFormType;
 
 class ProfilController extends AbstractController
 {
@@ -30,11 +31,17 @@ class ProfilController extends AbstractController
     public function register(Request $request,
                              UserPasswordEncoderInterface $passwordEncoder,
                              GuardAuthenticatorHandler $guardHandler,
-                             LoginFormAuthenticator $authenticator): Response
+                             LoginFormAuthenticator $authenticator,
+                                AuthenticationUtils $authenticationUtils): Response
 {
     $user = new User();
     $form = $this->createForm(RegistrationFormType::class, $user);
     $form->handleRequest($request);
+
+    // get the login error if there is one
+    $error = $authenticationUtils->getLastAuthenticationError();
+    // last username entered by the user
+    $lastUsername = $authenticationUtils->getLastUsername();
 
     if ($form->isSubmitted() && $form->isValid()) {
         // encode the plain password
@@ -58,8 +65,10 @@ class ProfilController extends AbstractController
         );
     }
 
-    return $this->render('security/register.html.twig', [
+    return $this->render('register.html.twig', [
         'registrationForm' => $form->createView(),
+        'last_username' => $lastUsername,
+        'error' => $error,
     ]);
 }
 
@@ -70,8 +79,10 @@ class ProfilController extends AbstractController
 {
     $user = $this->getUser();
 
+
     return $this->render('profil.html.twig', [
         'user' => $user,
+
     ]);
 }
 }
