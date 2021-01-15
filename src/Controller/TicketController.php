d6b0efab9183c7a4dbec0de5ac80c96c9a279f52
JeremyMordo <jeje.mordo@orange.fr>
2021-01-15 11:20:15 +0100

    update fixtures

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -109,10 +109,17 @@ class TicketController extends AbstractController
             $allGoodHeroes[]= $hero;
         }
 
+        $random_keys=array_rand($allGoodHeroes, 9);
+
+        for($i=0;$i<=8;$i++)
+        {
+        $nineHeroes []= $allGoodHeroes[$random_keys[$i]];
+        }
+
         return $this->render('result.html.twig', [
             'error' => $error,
             'last_username' => $lastUsername,
-            'allgoodheroes' => $allGoodHeroes,
+            'nineheroes' => $nineHeroes,
             'category' => $category
         ]);
     }
